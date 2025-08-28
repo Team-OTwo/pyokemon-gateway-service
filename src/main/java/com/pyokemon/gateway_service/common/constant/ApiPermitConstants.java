f7959d20a8280d305a5a2ae9706d0a2ef32a2b13
surnavy <113087821+surnavy@users.noreply.github.com>
2025-08-28 09:34:04 +0900

    [fix] Permit_All 경로 추가

--- src/main/java/com/pyokemon/gateway_service/common/constant/ApiPermitConstants.java
+++ src/main/java/com/pyokemon/gateway_service/common/constant/ApiPermitConstants.java
@@ -13,7 +13,8 @@ public final class ApiPermitConstants {
                 "/account/api/app/login",
                 "/account/api/users",
                 "/account/api/users/check-duplicate",
-                "/account/api/users/notification"
+                "/account/api/users/notification",
+                "/account/api/app/verify"
         };
     }
 
@@ -25,6 +26,13 @@ public final class ApiPermitConstants {
                 "/event/api/events/to-be-opened",
                 "/event/api/seats",
                 "/event/api/events/*"
+        };
+    }
+
+    // 결제 관련 허용 엔드포인트
+    public static class PaymentApi {
+        public static final String[] PERMIT_ALL = {
+                "/payment/api/payments/confirm-save"
         };
     }
 
